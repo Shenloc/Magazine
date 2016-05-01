d90fefd9975a769ca39d44eb5d1e3e3d50f6dd60
Shenloc <pbroniarczyk@outlook.com>
2016-05-01 11:03:57 +0200

    Added calendar with some js - not 100%

--- js/script.js
+++ js/script.js
@@ -4,13 +4,22 @@ function init() {
   $('#nav-dropdown').slideToggle(1);
 }
 
-
   $('#menu-button').click(function(){
     $(this).toggleClass('active-menu');
     $('#nav-dropdown').slideToggle(300);
   });
 
+/*
   $(".social-list a").on('hover', function(){
     var color = "#db4437";
     $(this, ".fa").css("color", "red");
+  });
+*/
+
+  //  CALENDAR
+
+  $('td').on('click', function(){
+    if ($('td').text().length > 0) {
+      $(this).toggleClass('mark');
+    }
   });
