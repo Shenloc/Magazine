55f7d2d4429e6042b4addb02cb435ac69b49c52b
Shenloc <pbroniarczyk@outlook.com>
2016-05-02 10:29:37 +0200

    Fixed last section - footer tabs.

--- js/script.js
+++ js/script.js
@@ -4,22 +4,30 @@ function init() {
   $('#nav-dropdown').slideToggle(1);
 }
 
+  //  NAVBAR
   $('#menu-button').click(function(){
     $(this).toggleClass('active-menu');
     $('#nav-dropdown').slideToggle(300);
   });
 
-/*
-  $(".social-list a").on('hover', function(){
-    var color = "#db4437";
-    $(this, ".fa").css("color", "red");
+
+  $(".nav-link").on("hover", function(){
+    $(this).children().css("color", "#fff");
   });
-*/
+
 
   //  CALENDAR
-
   $('td').on('click', function(){
     if ($('td').text().length > 0) {
       $(this).toggleClass('mark');
     }
+  });
+
+  //  TABS
+  $('#footer-nav a').on('click', function(e){
+    e.preventDefault();
+    $(this).addClass('active-tab').siblings().removeClass('active-tab');
+    var tab = $(this).attr('href');
+    $('.tab-content').not(tab).css('display', 'none');
+    $(tab).fadeIn();
   });
