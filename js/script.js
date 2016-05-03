5c92aad532c78f6a2aa1fadabbf5d0fde1c2ad54
Shenloc <pbroniarczyk@outlook.com>
2016-05-03 14:32:28 +0200

    minor fixes

--- js/script.js
+++ js/script.js
@@ -4,30 +4,14 @@ function init() {
   //  NAVBAR
   $('#menu-button').click(function(){
     $(this).toggleClass('active-menu');
-    $('#nav-dropdown').slideToggle(300).css('display', 'flex');
-  });
-
-
-  $(".social-list a").on("hover", function(){
-    $(this).children().css("background-color", "red");
+    $('#nav-dropdown').slideToggle(300);
   });
 
   //VIDEO PLAYER
-  /*var video = $('.fa-play-circle');
-  var cover = $('#video-thumb');
-  video.onclick = play;
-
-  function play (){
-    video.onclick = cover.style.display = "none";
-    document.getElementById('video-player').style.display = "block";
-    document.getElementById('video-source').setAttribute('src', 'https://player.vimeo.com/video/114953855?rel=0&autoplay=1')
-  }*/
-
   $('.fa-play-circle').on('click', function(){
     $('#video-thumb, .fa-play-circle').css('display', 'none');
     $('iframe').attr('src', 'https://player.vimeo.com/video/63977087?autoplay=1&byline=0&portrait=0');
   });
-
 
   //  CALENDAR
   $('td').on('click', function(){
